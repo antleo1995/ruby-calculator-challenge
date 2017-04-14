0393d2f3faa98d21a07c4240493de4b7d1a6afe2
antleo1995 <antleo1995@gmail.com>
2017-04-14 13:26:58 -0400

    Ruby Calculator challenge
    Got all requirements and chaining methods to work.
    
    Was this just easier or am I just getting better?

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,36 @@
 # frozen_string_literal: true
 
-class Calculator; end
+# calculator class
+class Calculator
+  def initialize(start_num)
+    @start_num = start_num
+  end
+
+  def add(number)
+    @start_num = @start_num += number.to_f
+    self
+  end
+
+  def subtract(number)
+    @start_num = @start_num -= number.to_f
+    self
+  end
+
+  def multiply(number)
+    @start_num = @start_num *= number.to_f
+    self
+  end
+
+  def divide(number)
+    @start_num = @start_num /= number.to_f
+    self
+  end
+
+  def equals
+    p @start_num.to_f
+  end
+
+  def clear
+    @start_num = 0
+  end
+end
